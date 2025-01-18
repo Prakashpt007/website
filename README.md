70a69c9dfd516b4c1f6cd27940230616eff00a4f
Prakash Patil <67907803+PRAKASHCROPDATA@users.noreply.github.com>
2025-01-18 15:37:54 +0530

    new project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Unified App Admin Frontend Dashboard
+# Website
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.0.6.
 
--- src/app/loading/loading.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-import { MatStepper } from "@angular/material/stepper";
-
-@Component({
-  selector: "app-loading",
-  templateUrl: "./loading.component.html",
-  styleUrls: ["./loading.component.scss"],
-})
-export class LoadingComponent { }
